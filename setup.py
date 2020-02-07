8b03399231dbc5cdf956b07f311d3186daccc121
Paul Nobrega <53446457+NobregaIPI@users.noreply.github.com>
2020-02-07 10:10:51 -0500

    license update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: Free For Educational Use",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
